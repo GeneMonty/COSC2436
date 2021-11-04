a7f47ecb33c59b419973e29c029f04a9f11ade73
GeneMonty <36083539+GeneMonty@users.noreply.github.com>
2021-11-04 13:09:36 -0500

    complete

---
+++ src/Week08/Sorting.java
@@ -0,0 +1,241 @@
+package Week08;
+
+//Java implementation of QuickSort 
+import java.io.*;
+
+public class Sorting {
+    // ************************************
+    // Selection Sort
+    // ************************************
+    public static void selectionSort(Integer[] arr) {
+        int n = arr.length;
+
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+        // Debug Print Array
+        // for (Integer x : arr)
+        // System.out.print(x + ", ");
+
+    }
+
+    // ************************************
+    // BubbleSort
+    // ************************************
+    public static void bubbleSort(Integer[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+        // // Debug Print Array
+        // for (Integer x : arr)
+        // System.out.print(x + ",");
+    }
+
+    // ************************************
+    // Merge
+    // ************************************
+    public static void merge(Integer[] Arr, int start, int mid, int end) {
+
+        // create a temp array
+        int temp[] = new int[end - start + 1];
+
+        // crawlers for both intervals and for temp
+        int i = start, j = mid + 1, k = 0;
+
+        // traverse both arrays and in each iteration add smaller of both elements in
+        // temp
+        while (i <= mid && j <= end) {
+            if (Arr[i] <= Arr[j]) {
+                temp[k] = Arr[i];
+                k += 1;
+                i += 1;
+            } else {
+                temp[k] = Arr[j];
+                k += 1;
+                j += 1;
+            }
+        }
+
+        // add elements left in the first interval
+        while (i <= mid) {
+            temp[k] = Arr[i];
+            k += 1;
+            i += 1;
+        }
+
+        // add elements left in the second interval
+        while (j <= end) {
+            temp[k] = Arr[j];
+            k += 1;
+            j += 1;
+        }
+
+        // copy temp to original interval
+        for (i = start; i <= end; i += 1) {
+            Arr[i] = temp[i - start];
+        }
+    }
+
+    // Arr is an array of integer type
+    // start and end are the starting and ending index of current interval of Arr
+
+    static void mergeSort(Integer[] Arr, int start, int end) {
+
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(Arr, start, mid);
+            mergeSort(Arr, mid + 1, end);
+            merge(Arr, start, mid, end);
+        }
+    }
+
+    // ************************************
+    // Quick
+    // ************************************
+    public static void quickSort(Integer arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(Integer arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i + 1;
+    }
+
+    // ************************************
+    // Heap
+    // ************************************
+
+    public static void heapSort(Integer[] arr) {
+        int n = arr.length;
+
+        // Build max heap
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // Heap sort
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Heapify root element
+            heapify(arr, i, 0);
+        }
+    }
+
+    static void heapify(Integer[] arr, int n, int i) {
+        // Find largest among root, left child and right child
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // Swap and continue heapifying if root is not largest
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+
+    // ************************************
+    // Radix
+    // ************************************
+
+    // Using counting sort to sort the elements in the basis of significant places
+    static void countingSort(Integer[] array, int size, int place) {
+        int[] output = new int[size + 1];
+        int max = array[0];
+        for (int i = 1; i < size; i++) {
+            if (array[i] > max)
+                max = array[i];
+        }
+        int[] count = new int[max + 1];
+
+        for (int i = 0; i < max; ++i)
+            count[i] = 0;
+
+        // Calculate count of elements
+        for (int i = 0; i < size; i++)
+            count[(array[i] / place) % 10]++;
+
+        // Calculate cumulative count
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        // Place the elements in sorted order
+        for (int i = size - 1; i >= 0; i--) {
+            output[count[(array[i] / place) % 10] - 1] = array[i];
+            count[(array[i] / place) % 10]--;
+        }
+
+        for (int i = 0; i < size; i++)
+            array[i] = output[i];
+    }
+
+    // Function to get the largest element from an array
+    static int getMax(Integer[] array, int n) {
+        int max = array[0];
+        for (int i = 1; i < n; i++)
+            if (array[i] > max)
+                max = array[i];
+        return max;
+    }
+
+    // Main function to implement radix sort
+    public static void radixSort(Integer[] array, int size) {
+        // Get maximum element
+        int max = getMax(array, size);
+
+        // Apply counting sort to sort elements based on place value.
+        for (int place = 1; max / place > 0; place *= 10)
+            countingSort(array, size, place);
+    }
+
+}
